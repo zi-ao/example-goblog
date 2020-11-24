aa3776feffc8fd84bbc0061ce970487b0b60397a
zi-ao <869822845@qq.com>
2020-11-24 16:32:33 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	fmt.Fprint(w, "请求路径为："+r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
