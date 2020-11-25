98743f14e399764fb8273d4742d776737eae2918
zi-ao <869822845@qq.com>
2020-11-25 10:47:03 +0800

    读取请求数据

--- main.go
+++ main.go
@@ -36,7 +36,10 @@ func aritlcesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func aritlcesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
