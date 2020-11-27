9637bb33b9fb54d067d1bbbb22e58379131182d4
zi-ao <869822845@qq.com>
2020-11-27 11:05:01 +0800

    模型基类和 view 包

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -4,19 +4,19 @@ import (
 	"goblog/pkg/logger"
 	"goblog/pkg/model"
 	"goblog/pkg/route"
-	"strconv"
 )
 
 // Article 文章模型
 type Article struct {
-	ID    int64
+	model.BaseModel
+
 	Title string
 	Body  string
 }
 
 // Link 方法用来生成文章链接
 func (a Article) Link() string {
-	return route.Name2URL("articles.show", "id", strconv.FormatInt(a.ID, 10))
+	return route.Name2URL("articles.show", "id", a.GetStringID())
 }
 
 // Create 创建文章，通过 article.ID 来判断是否创建成功
--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"goblog/pkg/logger"
+	"goblog/pkg/types"
 
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
@@ -9,6 +10,16 @@ import (
 	// GORM 的 MSYQL 数据库驱动导入
 	"gorm.io/driver/mysql"
 )
+
+// BaseModel 模型基类
+type BaseModel struct {
+	ID uint64
+}
+
+// GetStringID 获取 ID 的字符串格式
+func (a BaseModel) GetStringID() string {
+	return types.Uint64ToString(a.ID)
+}
 
 // DB gorm.DB 对象
 var DB *gorm.DB
